c7873ef7d79b472d7d98bdf6adb1642f50675114
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2025-07-08 18:50:18 -0300

    feat: add create question, create room and get room questions

---
+++ server/src/http/routes/create-question.ts
@@ -0,0 +1,37 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { z } from 'zod/v4';
+import { db } from '../../database/connection.ts';
+import { schema } from '../../database/schema/index.ts';
+
+export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    '/rooms/:roomId/questions',
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+        body: z.object({
+          question: z.string().min(1),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { roomId } = request.params;
+      const { question } = request.body;
+
+      const result = await db
+        .insert(schema.questions)
+        .values({ roomId, question })
+        .returning();
+
+      const createdQuestion = result[0];
+
+      if (!createdQuestion) {
+        throw new Error('Failed to create new question');
+      }
+
+      return reply.status(201).send({ questionId: createdQuestion.id });
+    }
+  );
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,9 @@ import {
 } from 'fastify-type-provider-zod';
 
 import { env } from './env.ts';
+import { createQuestionRoute } from './http/routes/create-question.ts';
+import { createRoomsRoute } from './http/routes/create-room.ts';
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -23,6 +26,9 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomsRoute);
+app.register(getRoomQuestionsRoute);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.info(`Port: ${process.env.PORT}`);
